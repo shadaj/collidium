25fb0805c8c6f1a7bdca7ac137f1304cc0a07004
shadaj <shadaj>
2013-07-22 13:27:55 -0700

    New levels and optimized js code

---
+++ js/startup.js
@@ -0,0 +1,7 @@
+jumpLevel = function() {
+  ScalaJS.modules.me\ufe33shadaj\ufe33collidium\ufe33Main().levelJump();
+}
+
+ScalaJS.modules.me\ufe33shadaj\ufe33collidium\ufe33Main().main();
+
+jumpLevel

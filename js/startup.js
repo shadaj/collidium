f8934bfd8d18314d8081781a07255729da7efcad
shadaj <shadaj>
2014-03-29 17:32:50 -0700

    PhysicsJS

--- js/startup.js
+++
@@ -1,7 +0,0 @@
-jumpLevel = function() {
-  ScalaJS.modules.me\ufe33shadaj\ufe33collidium\ufe33Main().levelJump();
-}
-
-ScalaJS.modules.me\ufe33shadaj\ufe33collidium\ufe33Main().main();
-
-jumpLevel
